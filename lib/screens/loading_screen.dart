8f53ad7d3e6fe0827e624e2f4f7af9bbe6033cd5
Israel Schroeder Moreira <israelschroederm@gmail.com>
2022-10-19 20:26:24 -0300

    Israel Schroeder Moreira

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tempo_template/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +10,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getData() async {
+    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  Future<void> getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print("Latitude: ${location.latitude}");
+    print("Longitude: ${location.longitude}");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
-      ),
     );
   }
 }
