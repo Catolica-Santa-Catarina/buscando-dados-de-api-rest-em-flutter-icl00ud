03dcf8edb1a8f65b18eecf2f85ee31e004222e94
Israel Schroeder Moreira <israelschroederm@gmail.com>
2022-10-26 20:02:58 -0300

    Israel Schroeder Moreira
    Finalização do aplicativo de tempo

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tempo_template/services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:tempo_template/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tempo_template/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -10,35 +11,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getData() async {
-    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-    http.Response response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      var data = response.body;
-      print(data);
-    } else {
-      print(response.statusCode);
-    }
+  void getData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+    pushToLocationScreen(weatherData);
   }
 
-  Future<void> getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print("Latitude: ${location.latitude}");
-    print("Longitude: ${location.longitude}");
+  void pushToLocationScreen(dynamic weatherData) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData);
+    }));
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
     getData();
-    return Scaffold(
+    return const Center(
+      child: SpinKitDoubleBounce(
+        color: Colors.white,
+        size: 100.0,
+      ),
     );
   }
 }
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,3 +1,9 @@
+import 'package:tempo_template/services/location.dart';
+import 'package:tempo_template/services/networking.dart';
+
+const apiKey = 'f37514c4de75aec88a4735cca786547c';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
   String getWeatherIcon(int condition) {
     if (condition < 300) {
@@ -29,5 +35,17 @@ class WeatherModel {
     } else {
       return 'Leve um 🧥';
     }
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}'
+            '&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
   }
 }
